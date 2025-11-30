3d18ed4c09a5e78b2aa05257553d351e804756a4
Kamta Rawlings <kamtarawlings@gmail.com>
2025-11-30 14:06:39 +0100

    modified the grading file and completed the circleareadiameter

--- chap1_Python_Tutorials/Grading.py
+++ chap1_Python_Tutorials/Grading.py
@@ -1,11 +1,11 @@
 '''
+QUESTION
 Docstring for chap1-Introduction.Grading
-Take in the grades from the user using the 
-input function and print out the following statement 
+Take in the grades from the user using the
+input function and print out the following statement
 based on the above grading scheme
 
 “Your grade of Y earns you an X in this course”
-
 where X stands for one of the four letter grades,
 A to D, and Y is the grades received as input from
 the user, which you can assume will always be between
@@ -14,17 +14,24 @@ to 1
 
 [Hint: use if statements.]
 '''
-A -> 90 and above
-B -> 70 and above
-C -> 50 and above
-D -> 0-50
+# A -> 90 and above
+# B -> 70 and above
+# C -> 50 and above
+# D -> 0-50
 
-grade = input(int("Enter the student grade: "))
+grade = int(input("Enter the student grade: "))
 if grade > 70:
     if grade > 90:
-        print (f'Your grade of {grade} earns you an A in this course')
+        print(f'Your grade of {grade} earns you an A in this course')
     else:
-        print (f'Your grade of {grade} earns you an A in this course')
+        print(f'Your grade of {grade} earns you an B in this course')
 elif grade > 50:
-    print
-    
+    print(f'Your grade of {grade} earns you an C in this course')
+else:
+    print(f'Your grade of {grade} earns you an D in this course')
+
+'''
+I think you can use if 4 times to print
+out thesame result but this looks and professionl
+The challenge here was using if and elif to have the desired results
+'''
