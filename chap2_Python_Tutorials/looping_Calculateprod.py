5088157d28d9fe5d8fbb5c1b51a51643705280fb
Kamta Rawlings <kamtarawlings@gmail.com>
2025-12-03 21:39:36 +0100

    added the coe to calculateprod looping

--- chap2_Python_Tutorials/looping_Calculateprod.py
+++ chap2_Python_Tutorials/looping_Calculateprod.py
@@ -1,4 +1,11 @@
 '''
 Write a program that, given a value x
-,calculates the value
+,calculates the value x ^ x
+'''
+num = int(input('Enter the number you want to raise: '))
+output = num ** num
+print(output)
+
+'''
+This is just a loop, donot over think.
 '''
