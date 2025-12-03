8ea6a3809adfebf93c8d3387cf563ca90d4eb5b1
Kamta Rawlings <kamtarawlings@gmail.com>
2025-12-03 22:02:35 +0100

    Looping calculate, calculating x factorial

---
+++ chap2_Python_Tutorials/looping_calfactorial.py
@@ -0,0 +1,15 @@
+'''
+Write a program that, given a value x, calculates the value
+"X factorial"
+'''
+factorial = 1
+num = input('Enter the number you want to do factorial: ')
+numb = int(num)
+for i in range(1, numb + 1):
+    factorial *= i
+print(factorial)
+
+'''
+The main aim here is to understand the what the question
+is and try to loop around it.
+'''
